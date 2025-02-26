506d08126a6805d75d3e5b4ba72271ba36728478
Albert735 <03jayteee@gmail.com>
2025-02-26 20:56:18 +0000

    environment setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "@/components/ui/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home</h1>
+    </div>
+  );
+}
