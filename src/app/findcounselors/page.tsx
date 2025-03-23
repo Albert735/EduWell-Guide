26c9e879604f22ad8a7f3ee8802277c81cfde208
Albert735 <03jayteee@gmail.com>
2025-03-23 02:06:54 +0000

    feat: Enhance Find Counselors page with new informational sections and improved layout

--- src/app/findcounselors/page.tsx
+++ src/app/findcounselors/page.tsx
@@ -5,6 +5,7 @@ import dynamic from "next/dynamic";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import { GoArrowUpRight } from "react-icons/go";
 
 // Dynamically import FaStar with SSR disabled
 const FaStar = dynamic(
@@ -71,7 +72,7 @@ const counselors: CounselorType[] = [
 
 export default function FindcounselorsPage() {
   return (
-    <section className="container flex flex-col justify-center items-center max-w-screen-xl mx-auto p-4 gap-[5rem] py-[5rem]">
+    <section className="container flex flex-col justify-center items-center max-w-screen-xl mx-auto p-4 gap-[7rem] py-[5rem]">
       <header className="text-center space-y-10">
         <h1 className="text-4xl md:text-5xl font-bold">
           Your Trusted Guide to Academic <br /> & Personal Success
@@ -133,6 +134,40 @@ export default function FindcounselorsPage() {
             </Card>
           </div>
         ))}
+      </div>
+
+      <div className="flex justify-between items-start gap-10 w-full">
+        <span className="flex flex-col gap-4">
+          <span className="flex items-center gap-2">
+            <h2 className="text-md font-medium">Find Counselor</h2>{" "}
+            <GoArrowUpRight />
+          </span>
+
+          <p className="text-gray-400 text-sm">
+            For guidance when registering via the website
+          </p>
+        </span>
+        <span className="flex flex-col gap-4">
+          <span className="flex items-center gap-2">
+            <h2 className="text-md font-medium">Seamless Consultation</h2>{" "}
+            <GoArrowUpRight />
+          </span>
+
+          <p className="text-gray-400 text-sm">
+            For all types of services available here
+          </p>
+        </span>
+        <span className="flex flex-col gap-4">
+          <span className="flex items-center gap-2">
+            <h2 className="text-md font-medium">Get Your Solution</h2>
+            <GoArrowUpRight />
+          </span>
+
+          <p className="text-gray-400 text-sm">
+            Here,we will assess, create a plan of care, <br /> and consult the
+            best certified counselors
+          </p>
+        </span>
       </div>
 
       {/* CTA Section */}
