3dafe94cc38f897c55fd9dc5fbc9ed6c8c81e9b0
Albert735 <03jayteee@gmail.com>
2025-04-30 15:52:40 +0000

    refactor: remove unused Calendar component and related dependencies

--- src/app/dashboard/counselor/page.tsx
+++ src/app/dashboard/counselor/page.tsx
@@ -5,12 +5,8 @@ import AppointRequest from "./(components)/AppointRequest";
 import Overview from "./(components)/Overview";
 import PatientOverview from "./(components)/PatientOverview";
 import ChartGrid from "./(components)/chartGrid";
-import { Calendar } from "@/components/ui/calendar";
-import { Card } from "@/components/ui/card";
 
 export default function CounselorDashboard() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
-
   return (
     <div className="flex flex-col gap-4 p-4 overflow-y-auto lg:grid lg:grid-cols-4 lg:grid-rows-5 lg:h-[calc(100vh-5.5rem)]">
       {/* Overview Section */}
@@ -22,14 +18,6 @@ export default function CounselorDashboard() {
       <div className="col-span-2 row-span-2 col-start-3 flex flex-col gap-4">
         <div className="flex flex-col xl:flex-row justify-between gap-4 h-full w-full">
           <ChartGrid />
-          <Card className="p-4">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              className="rounded-md"
-            />
-          </Card>
         </div>
       </div>
 
